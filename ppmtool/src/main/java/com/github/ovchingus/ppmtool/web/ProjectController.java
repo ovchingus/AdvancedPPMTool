23d6f649325fbbe9bfbaaea8995c6a912f61a701
Vladimir Ovechkin <ovchingus@gmail.com>
2019-03-19 23:50:06 +0300

    Upgrade validation error response for Project object

--- ppmtool/src/main/java/com/github/ovchingus/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/github/ovchingus/ppmtool/web/ProjectController.java
@@ -6,12 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/project")
@@ -26,9 +32,12 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-
+        
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(result.getFieldErrors().stream().collect(Collectors.toMap(
+                    o -> o.getField(),
+                    o -> o.getDefaultMessage())), HttpStatus.BAD_REQUEST);
+
         }
 
         projectService.saveOrUpdate(project);
