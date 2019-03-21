2666bd25ac3a1516bc0e8fdb1c4d22d6cfcf8e8e
Vladimir Ovechkin <ovchingus@gmail.com>
2019-03-22 00:52:52 +0300

    Add exceptions for unique Project identifiers

---
+++ ppmtool/src/main/java/com/github/ovchingus/ppmtool/exceptions/ProjectIdExceptionResponce.java
@@ -0,0 +1,10 @@
+package com.github.ovchingus.ppmtool.exceptions;
+
+public class ProjectIdExceptionResponce {
+
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponce(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
--- ppmtool/src/main/java/com/github/ovchingus/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/github/ovchingus/ppmtool/services/ProjectService.java
@@ -1,9 +1,14 @@
 package com.github.ovchingus.ppmtool.services;
 
 import com.github.ovchingus.ppmtool.domain.Project;
+import com.github.ovchingus.ppmtool.exceptions.CustomResponseEntityExceptionHandler;
+import com.github.ovchingus.ppmtool.exceptions.ProjectIdException;
 import com.github.ovchingus.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -16,7 +21,13 @@ public class ProjectService {
     }
 
     public Project saveOrUpdate(Project project) {
-        return projectRepository.save(project);
+        try {
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("ProjectId '" +
+                    project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
+
 
 }
