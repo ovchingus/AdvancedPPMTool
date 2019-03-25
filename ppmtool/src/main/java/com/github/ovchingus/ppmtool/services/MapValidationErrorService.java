3f3abbcc4a759f3bea88503ba703fc04db62d5f9
Vladimir Ovechkin <ovchingus@gmail.com>
2019-03-25 22:09:53 +0300

    Bug fix.
    ErrorMap inflicts exception with the same key errors. Now there is only first error makes sense.

--- ppmtool/src/main/java/com/github/ovchingus/ppmtool/services/MapValidationErrorService.java
+++ ppmtool/src/main/java/com/github/ovchingus/ppmtool/services/MapValidationErrorService.java
@@ -4,7 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -13,10 +16,11 @@ public class MapValidationErrorService {
 
     public Optional<ResponseEntity<?>> MapValidationService(BindingResult result) {
         if (result.hasErrors()) {
-
             return Optional.of(new ResponseEntity<>(result.getFieldErrors().stream().collect(Collectors.toMap(
                     o -> o.getField(),
-                    o -> o.getDefaultMessage())), HttpStatus.BAD_REQUEST));
+                    o -> o.getDefaultMessage(),
+                    (firstError, otherErrors) -> firstError // ignore adding the same key to map
+                    )), HttpStatus.BAD_REQUEST));
         }
         return Optional.empty();
 
