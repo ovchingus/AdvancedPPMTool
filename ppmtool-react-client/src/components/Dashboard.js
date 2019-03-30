8771a7805327e11666989a94675bba3030f657b3
Vladimir Ovechkin <ovchingus@gmail.com>
2019-03-31 02:29:22 +0300

    Remove old testing stuff

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -13,11 +13,6 @@ class Dashboard extends Component {
   render() {
     const projects = this.props.project.projects;
 
-    const projectObject = {
-      projectName: "ProjectName PROPS",
-      projectIdentifier: "PROP",
-      description: "description from PROPS"
-    };
     return (
       <div className="projects">
         <div className="container">
