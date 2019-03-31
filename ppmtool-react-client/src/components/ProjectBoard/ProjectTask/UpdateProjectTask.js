c1e574d49b72a0b0dc49fb166eeaf2dda4b80696
Vladimir Ovechkin <ovchingus@gmail.com>
2019-03-31 19:56:38 +0300

    Fix UpdateProjectTask submit button and handle empty field taskName error

--- ppmtool-react-client/src/components/ProjectBoard/ProjectTask/UpdateProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTask/UpdateProjectTask.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import classnames from "classnames";
-import { getProjectTask } from "../../../actions/backlogActions";
+import {
+  getProjectTask,
+  updateProjectTask
+} from "../../../actions/backlogActions";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class UpdateProjectTask extends Component {
   constructor() {
@@ -17,7 +21,8 @@ class UpdateProjectTask extends Component {
       priority: "",
       dueDate: "",
       projectIdentifier: "",
-      createAt: ""
+      createAt: "",
+      errors: {}
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -29,6 +34,9 @@ class UpdateProjectTask extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
     const {
       id,
       projectSequence,
@@ -71,17 +79,27 @@ class UpdateProjectTask extends Component {
       projectIdentifier: this.state.projectIdentifier,
       createAt: this.state.createAt
     };
+    this.props.updateProjectTask(
+      this.state.projectIdentifier,
+      this.state.projectSequence,
+      UpdateProjectTask,
+      this.props.history
+    );
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="add-PBI">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
-              <a href="#" className="btn btn-light">
+              <Link
+                to={`/projectBoard/${this.state.projectIdentifier}`}
+                className="btn btn-light"
+              >
                 Back to Project Board
-              </a>
+              </Link>
               <h4 className="display-4 text-center">
                 Add /Update Project Task
               </h4>
@@ -93,12 +111,17 @@ class UpdateProjectTask extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.summary
+                    })}
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
                     onChange={this.onChange}
                   />
+                  {errors.summary && (
+                    <div className="invalid-feedback">{errors.summary}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <textarea
@@ -162,14 +185,17 @@ class UpdateProjectTask extends Component {
 
 UpdateProjectTask.propTypes = {
   getProjectTask: PropTypes.func.isRequired,
-  project_task: PropTypes.object.isRequired
+  project_task: PropTypes.object.isRequired,
+  updateProjectTask: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  project_task: state.backlog.project_task
+  project_task: state.backlog.project_task,
+  errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
-  { getProjectTask }
+  { getProjectTask, updateProjectTask }
 )(UpdateProjectTask);
