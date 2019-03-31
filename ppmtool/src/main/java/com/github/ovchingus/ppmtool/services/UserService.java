ac519bfff55411e9751d8cf360cb82867943f7cc
Vladimir Ovechkin <ovchingus@gmail.com>
2019-03-31 22:05:09 +0300

    Add User Object, Validation, Repository, Service

---
+++ ppmtool/src/main/java/com/github/ovchingus/ppmtool/services/UserService.java
@@ -0,0 +1,17 @@
+package com.github.ovchingus.ppmtool.services;
+
+import com.github.ovchingus.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+}
